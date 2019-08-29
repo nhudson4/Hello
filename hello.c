e37c98f2b9d8bf61803ed9d78713083a245e1669
nhudson4 <54552718+nhudson4@users.noreply.github.com>
2019-08-29 09:31:20 -0500

    Create hello.c
    added hello.c file with name and major print lines

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Noah Hudson
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Noah Hudson\n");
+  printf("Undeclared/Undecided"\n)
+
+  return 0;
+}
